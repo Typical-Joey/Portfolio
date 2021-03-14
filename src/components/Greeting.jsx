5a391209babe1294bddec885070d360661c8c54b
Typical-Joey <jtcapocci@yahoo.com>
2021-03-14 16:33:08 -0500

    Updated greeting

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+
+
 export default function Hello() {
   return (
     <div id="Greeting" className="greeting">
-      <h1>Hi, I'm Joseph</h1>
-      <h2>A Full Stack Software Engineer</h2>
+      <div className="greeting-text">
+        <h1>Hi, I'm Joseph</h1>
+        <h2>
+          A Full Stack Software Engineer
+        </h2>
+      </div>
     </div>
   );
 }
