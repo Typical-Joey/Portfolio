c4aa55643d0ed3bf14f14a5cdd5551101ddb9a2c
Typical-Joey <jtcapocci@yahoo.com>
2021-03-17 19:47:54 -0500

    Side icon links work (email and resume are not setup yet)

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import ProjectModal from "./ProjectModal";
 
 export default function Project(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  function handleClick() {
+    setShowModal(true);
+    <ProjectModal />;
+  }
+
   return (
-    <div className="card project">
+    <div className="card project" onClick={handleClick}>
       <div className={props.className}>
         <h1>{props.title}</h1>
       </div>
--- src/components/SideIcons.jsx
+++ src/components/SideIcons.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
-import EmailIcon from '@material-ui/icons/Email';
-import DescriptionIcon from '@material-ui/icons/Description';
-
+import EmailIcon from "@material-ui/icons/Email";
+import DescriptionIcon from "@material-ui/icons/Description";
 
 export default function SideIcons() {
   return (
     <div id="Side-Icons" className="side-icons">
-      <LinkedInIcon fontSize="large" />
-      <GitHubIcon fontSize="large" />
-      <EmailIcon fontSize="large" />
-      <DescriptionIcon fontSize="large"/>
+      <a href="https://www.linkedin.com/in/joseph-capocci-01bb10199/">
+        <LinkedInIcon fontSize="large" className="icon" />
+      </a>
+      <a href="https://github.com/Typical-Joey/">
+        <GitHubIcon fontSize="large" className="icon" />
+      </a>
+      <a href="">
+        <EmailIcon fontSize="large" className="icon" />
+      </a>
+      <a href="">
+        <DescriptionIcon fontSize="large" className="icon" />
+      </a>
     </div>
   );
 }
