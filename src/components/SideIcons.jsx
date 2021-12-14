19bffc38e7ae6403a347791f40fd2f5f2fae02e7
Typical-Joey <jtcapocci@yahoo.com>
2021-12-14 15:40:41 -0600

    Update pdf

--- src/components/SideIcons.jsx
+++ src/components/SideIcons.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import DescriptionIcon from "@material-ui/icons/Description";
-import pdf from "./josephCapocciResume.pdf";
+import pdf from "./josephCapocciResume.PDF";
 
 export default function SideIcons() {
   return (
