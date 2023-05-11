3a22d79db9cdda4cd40384fd48aee6f14da133ce
Joseph Capocci <typicaljoey@Josephs-MacBook-Pro.local>
2023-05-11 18:46:51 -0500

    REF:  Removed unused fragment

--- src/components/project/DemoButtons.jsx
+++ src/components/project/DemoButtons.jsx
@@ -4,21 +4,17 @@ import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 export const DemoActive = function ({ url }) {
   return (
-    <>
-      <a className="btn btn-outline-dark" href={url}>
-        <VisibilityIcon /> Demo
-      </a>
-    </>
+    <a className="btn btn-outline-dark" href={url}>
+      <VisibilityIcon /> Demo
+    </a>
   );
 };
 
 export const DemoInactive = function () {
   return (
-    <>
-      <a className="btn btn-outline-dark demo-inactive" href>
-        <VisibilityOffIcon />
-        Sorry
-      </a>
-    </>
+    <a className="btn btn-outline-dark demo-inactive" href>
+      <VisibilityOffIcon />
+      Sorry
+    </a>
   );
 };
