7499745bb36586f5961abcbe5749653adb076f2d
Joseph Capocci <typicaljoey@Josephs-MBP.attlocal.net>
2023-05-14 13:07:48 -0500

    REF:  Removed console logs and comments

--- src/service/EmailService.js
+++ src/service/EmailService.js
@@ -7,19 +7,12 @@ export async function sendEmail(message) {
       process.env.REACT_APP_SERVICE_ID,
       process.env.REACT_APP_TEMPLATE_ID,
       message,
-      // process.env.REACT_APP_USER_ID
-      "test"
+      process.env.REACT_APP_USER_ID
     )
     .then((result) => {
-      console.log("Result: " + result);
-      console.log(result.text);
-      console.log(result.status);
       return true;
     })
     .catch((error) => {
-      console.log("Error: " + error);
-      console.log("Error Text: ", error.text);
-      console.log(error.status);
       return false;
     });
 }
