a839eeebe0e45b7adaca81a9e53d32cafec928f4
Joseph Capocci <typicaljoey@Josephs-MBP.attlocal.net>
2023-05-14 13:32:59 -0500

    App:  Removed test code from previous commit

--- src/components/homepage/Greeting.jsx
+++ src/components/homepage/Greeting.jsx
@@ -5,7 +5,6 @@ export default function Hello() {
     <div id="Greeting" className="greeting">
       <div className="greeting-text">
         <h1>Hi, I'm Joseph</h1>
-        <h2>Test To see if production automatically updates</h2>
         <h2>A Full-Stack Software Engineer</h2>
       </div>
     </div>
